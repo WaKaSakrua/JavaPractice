cda76a85e93b96a35dc5550c5f5964472d70a2b6
LZY <2219628417@qq.com>
2026-06-23 15:09:48 +0800

    定义科研招募积分规则

--- main/java/com/course/service/ResearchRecruitment.java
+++ main/java/com/course/service/ResearchRecruitment.java
@@ -11,6 +11,9 @@ import com.course.utils.JsonUtils;
 //类名与方法名须与controller层拦截的方法一致
 public class ResearchRecruitment {
 
+    //参加科研招募获得的可兑换积分，无限制
+    private static final int RESEARCH_RECRUITMENT_SCORE = 8;
+
     public void researchRecruitment(){
         System.out.println("+++++researchRecruitment积分计算方法执行+++++");
     }
