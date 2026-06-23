367b5453e160dfa0468604bd6406448f82b22d24
LZY <2219628417@qq.com>
2026-06-23 09:35:11 +0800

    定义门诊随访积分规则

--- main/java/com/course/service/FollowUp.java
+++ main/java/com/course/service/FollowUp.java
@@ -11,6 +11,9 @@ import com.course.utils.JsonUtils;
 //类名与方法名须与controller层拦截的方法一致
 public class FollowUp {
 
+    //完成门诊随访获得的可兑换积分，无限制
+    private static final int FOLLOW_UP_SCORE = 3;
+
     public void followUp(){
         System.out.println("+++++followUp积分计算方法执行+++++");
     }
