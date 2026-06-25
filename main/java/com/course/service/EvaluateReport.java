136af421f8b354aaba9fe3663a09784ab841cbc9
theo <theo@local>
2026-06-25 11:08:58 +0800

    定义生成评估报告积分规则

--- main/java/com/course/service/EvaluateReport.java
+++ main/java/com/course/service/EvaluateReport.java
@@ -11,6 +11,11 @@ import com.course.utils.JsonUtils;
 //类名与方法名须与controller层拦截的方法一致
 public class EvaluateReport {
 
+    //生成评估报告获得的成长积分：2分
+    private static final int EVALUATE_REPORT_SCORE = 2;
+    //生成评估报告的前置条件：血糖记录数需大于等于10
+    private static final int REQUIRED_BLOOD_SUGAR_COUNT = 10;
+
     public void evaluateReport(){
         System.out.println("+++++evaluateReport积分计算方法执行+++++");
     }
