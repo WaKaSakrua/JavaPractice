ccfe2b764de917b38164968504231428af1c3911
theo <theo@local>
2026-06-25 11:27:56 +0800

    添加生成评估报告积分计算入口

--- main/java/com/course/service/EvaluateReport.java
+++ main/java/com/course/service/EvaluateReport.java
@@ -18,6 +18,13 @@ public class EvaluateReport {
 
     public void evaluateReport(){
         System.out.println("+++++evaluateReport积分计算方法执行+++++");
+        //读取积分文件
+        String file = FileUtils.readFile("score");
+        PointObject pointObject = JsonUtils.jsonToPojo(file, PointObject.class);
+        //TODO 后续补充积分累加与前置条件(已填资料、血糖记录数>=10)限制逻辑
+        //写回积分文件
+        String content = JsonUtils.objectToJson(pointObject);
+        FileUtils.writeFile("score", content);
     }
 
 }
